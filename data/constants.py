839c4cab3ef0e4256a24f072f1672dd50fe80e08
Willam Tran <quangminhtran2006@gmail.com>
2026-07-01 00:31:08 +0700

    Update feature extractors list

--- data/constants.py
+++ data/constants.py
@@ -2,8 +2,14 @@ FEATURE_EXTRACTORS = [
     'noise_residual_extract',
     'wavelet_extract',
     'fft_extract',
-    # 'dct_extract',
+    'dct_extract',
     'lbp_extract',
     'glcm_extract',
-    'stat_extract'
+    'stat_extract',
+    'gsm_extract',
+    'laplacian_stat_extract',
+    'hprb_extract',
+    'hog_extract',
+    'color_hist_extract',
+    'chroma_correlation_extract'
 ]
